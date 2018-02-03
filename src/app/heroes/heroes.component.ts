b868b12d250d35ad2eff46c3c22dc9b565e288c8
moon <mwj1st@naver.com>
2018-02-03 18:28:56 +0900

    20180203_1

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,23 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
 import {HEROES} from '../mock-heroes';
+import {HeroService} from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
-  constructor() {
+  // 2. 생성자로 서비스를 주입 받는다.
+  constructor(public heroService: HeroService) {
     // this.heroes = new Hero(1, 'Win Storm');
     /*    this.heroes.id = 1;
         this.heroes.name = 'Win Storm';*/
   }
 
   ngOnInit() {
+    // 비동기 처리
+    this.heroService.getHeroes()
+      .subscribe(data => {
+        this.heroes = data;
+      });
   }
 
   onSelect(hero: Hero) {
